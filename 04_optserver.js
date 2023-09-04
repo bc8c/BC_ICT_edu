a0000676bb948b27334574764cbcb662de8eced2
bc8c <35796622+bc8c@users.noreply.github.com>
2023-09-04 22:22:25 +0900

    router

---
+++ 04_optserver.js
@@ -0,0 +1,31 @@
+
+
+const bodyParser = require("body-parser");
+const express = require("express");
+const path = require("path");
+
+const bodyrouter = require("./routes/body")
+
+const app = express();
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+app.use(express.static(path.join(__dirname, "views")));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/body", bodyrouter);
+
+//-----------------------------------------------라우터 
+app.get('/aaa/bbb', (req, res)=>{
+    const name = req.query.name
+    const age = req.query.age
+    
+    console.log("name : " + name)
+    console.log("age : " + age)
+    res.send(`<h1>name : ${name}, age : ${age}</h1>`)
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
