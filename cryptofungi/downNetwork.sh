249d14a46ad9ac72adc1297924fa8e9aaecbe88d
bc8c <bc8c@naver.com>
2023-09-12 13:03:06 +0000

    cryptoFungi Project : Network

---
+++ cryptofungi/downNetwork.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pushd network
+
+./networkdown.sh
+
+popd
---
+++ cryptofungi/startNetwork.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pushd network
+
+./startnetwork.sh
+sleep 5
+
+./createchannel.sh
+sleep 5
+
+./setAnchorPeerUpdate.sh
+
+popd
