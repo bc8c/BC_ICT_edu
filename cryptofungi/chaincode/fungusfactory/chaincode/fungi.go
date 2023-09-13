cbaa6a95054c25dfedd39a11b41c8f30f8028a35
bc8c <bc8c@naver.com>
2023-09-13 12:46:19 +0000

    chaincode base

---
+++ cryptofungi/chaincode/fungusfactory/chaincode/fungi.go
@@ -0,0 +1,9 @@
+package chaincode
+
+import ( 
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct{
+	contractapi.Contract
+}
---
+++ cryptofungi/chaincode/fungusfactory/go.mod
@@ -0,0 +1,5 @@
+module fungusfactory
+
+go 1.14
+
+require github.com/hyperledger/fabric-contract-api-go v1.1.0
