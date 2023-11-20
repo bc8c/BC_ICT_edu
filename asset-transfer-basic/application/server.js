fb1787543658925d55e5e9cb3f1e6879f2e5c725
bc8c <bc8c@naver.com>
2023-11-20 01:04:35 +0000

    crateAsset page

--- asset-transfer-basic/application/server.js
+++ asset-transfer-basic/application/server.js
@@ -16,11 +16,6 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// app.get("/welcom", (req, res) =>{
-//     const num = req.query.num
-//     res.render("welcom",{count:num, name:"홍길동"})
-// })
-
 app.get("/", (req,res)=>{
     res.render("index")
 })
@@ -29,6 +24,29 @@ app.get("/asset/create", (req,res)=>{
     res.render("assetcreate")
 })
 
+app.get("/asset/read", (req,res)=>{
+    res.render("assetread")
+})
+
+app.post("/asset/create", (req,res)=>{
+    const id = req.body.ID
+    const color = req.body.COLOR
+    const size = req.body.SIZE
+    const owner = req.body.OWNER
+    const apvalue = req.body.APVALUE
+    console.log("in /asset/create")
+    console.log(id, color, size, owner, apvalue)
+
+    // 자산생성 수행
+    // 블록체인 네트워크에 있는 asset-transfer-basic 체인코드 호출
+    // 체인코드내에 있는 CreateAsset 함수를 호출해야함
+
+    // 블록체인 연동후 체인코드 호출이 정상적으로 잘 처리된경우 
+
+    // 그 실행 결과를 받아와서 response를 만들어 웹 브라우저에 전달
+    res_str = `{"resultcode":"success", "msg":"자산생성이 정상적으로 완료되었습니다."}`
+    res.json(JSON.parse(res_str))
+})
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
