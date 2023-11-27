d78168f547606e5ab0d6ad6ef6808fcf774fd0c8
bc8c <bc8c@naver.com>
2023-11-27 13:09:31 +0000

    GET data

--- expressserver/03_getresserver.js
+++ expressserver/03_getresserver.js
@@ -8,12 +8,15 @@ const PORT = 3000
 const HOST = "0.0.0.0"
 
 
-app.get("/bc_class", ()=>{
-    console.log("in /bc_class")
+app.get("/bc_class", (req, res)=>{
+    
+    console.log(req.query.name)
+    console.log(req.query.age)
+    res.send(`환영합니다 블록체인 클래스 수강생(이름:${req.query.name},나이:${req.query.age})`)
 })
 
-app.get("/test", ()=>{
-    console.log("in /test")
+app.get("/test", (req, res)=>{
+    console.log(req)
 })
 
 app.listen(PORT, HOST)
